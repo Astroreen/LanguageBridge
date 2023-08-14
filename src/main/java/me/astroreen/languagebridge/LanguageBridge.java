d479f5a53bda9cbd5b5b1a4335bc911889dd7e1a
Astroreen <ilja.zoludev@gmail.com>
2023-08-14 13:53:11 +0300

    Initial commit

---
+++ src/main/java/me/astroreen/languagebridge/LanguageBridge.java
@@ -0,0 +1,17 @@
+package me.astroreen.languagebridge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LanguageBridge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
